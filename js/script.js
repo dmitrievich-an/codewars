01f9ab7c17cb0c034b997fc53957ecc212c95b9a
Artem <reebok001@yandex.ru>
2022-12-30 10:11:33 +0300

    added two solutions

--- js/script.js
+++ js/script.js
@@ -47,3 +47,33 @@
 //     return total;
 //   }
 // }
+
+// TODO: Is n divisible by x and y?
+// DESCRIPTION:
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+//
+// Examples:
+// 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+// 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+// 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+// 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+//
+//
+// SOLUTION:
+// const isDivisible = (n, x, y) => n % x === 0 && n % y === 0;
+
+// TODO: Convert a String to a Number!
+// DESCRIPTION:
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+//
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+//
+// Examples:
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+//
+//
+// SOLUTION:
+// const stringToNumber = (str) => +str
