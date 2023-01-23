ffe305264ab89f9203bc7b8ac2e54a06c6c4ba1d
Artem <reebok001@yandex.ru>
2023-01-23 13:28:48 +0300

    added one solution

--- js/script.js
+++ js/script.js
@@ -110,3 +110,29 @@
 // };
 //
 // console.log(smash(words));
+
+// TODO: Find the next perfect square!
+// DESCRIPTION:
+// You might know some pretty large perfect squares. But what about the NEXT one?
+//
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+//
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+//
+// Examples:(Input --> Output)
+//
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+//
+// SOLUTION:
+// function findNextSquare(sq) {
+//   const squareRoot = Math.sqrt(sq);
+//   if (squareRoot >= 0 && Number.isInteger(squareRoot)) {
+//     return (squareRoot + 1) * (squareRoot + 1)
+//   } else {
+//     return -1;
+//   }
+// }
+//
+// console.log(findNextSquare(120))
